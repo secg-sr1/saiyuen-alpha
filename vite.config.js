d79fc5d188458d9e822ec2fb03c613fc62b16f9c
secg-sr1 <scalgua89@gmail.com>
2024-10-23 19:44:48 -0600

    bridge material

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import ghPages from 'vite-plugin-gh-pages'
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages],
-  base: '/repository',
+  plugins: [
+    react(),
+    topLevelAwait(),
+  ],
+  base: '/saiyuen-alpha',
 })
